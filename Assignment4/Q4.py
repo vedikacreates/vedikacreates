c3d05be81c8d04c90d2f9ee1d1708d63634f9927
Vedika Bhingole <vedikabhingole@gmail.com>
2026-06-26 21:18:52 +0530

    Delete Assignment4 directory

--- Assignment4/Q4.py
+++
@@ -1,7 +0,0 @@
-#factorial of a number
-
-num = int(input('Enter Number:'))
-fact = 1
-for i in range(1,num+1):
-    fact = fact * i
-print(fact)
--- Assignment4/Q8.py
+++
@@ -1,8 +0,0 @@
-#WAP to print all numbers in a range divisible by given number
-
-n = int(input('Enter n:'))
-num = 0
-for i in range(1,25):
-    num = i % n
-    if(num == 0):
-        print(i)
