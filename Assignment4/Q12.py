9aa94df4ad6f7bd1fcc4bd7ca602f52378f16741
Vedika Bhingole <vedikabhingole@gmail.com>
2026-07-14 23:15:38 +0530

    Q12.py

--- Assignment4/Q12.py
+++ Assignment4/Q12.py
@@ -9,12 +9,12 @@ while (temp > 0):
     temp = temp // 10
     count += 1
 sum = 0
-num = temp
-while(num > 0):
+temp = sum
+while(temp > 0):
     d = temp % 10
     sum = sum + (d**count)
     temp = temp // 10
-if(sum == temp):
+if(sum == num):
     print('It is a Armstrong number.')
 else:
     print('It is not a Armstrong number.')
