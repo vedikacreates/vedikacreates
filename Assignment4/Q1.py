ded9978d10f24a119659bab26e693b0351ab0cee
Vedika Bhingole <vedikabhingole@gmail.com>
2026-07-14 23:23:25 +0530

    Add files via upload

---
+++ Assignment4/Q1.py
@@ -0,0 +1,5 @@
+#print all even numbers until n
+
+n = int(input('Enter n:'))
+for i in range(2,n+1,2):
+    print(i)
---
+++ Assignment4/Q2.py
@@ -0,0 +1,5 @@
+#print all odd numbers until n
+
+n = int(input('Enter n:'))
+for i in range(1,n+1,2):
+    print(i)
